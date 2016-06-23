6c6d2717686b999e107a3c66293f659b7b90f644
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-06-23 12:00:46 +0100

    Upgraded eclim to Eclipse Neon

--- eclimd/eclimd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-Xvfb :1 -screen 0 1024x768x24 &
-DISPLAY=:1 /usr/local/eclipse/eclimd -b
--- eclimd/run-eclimd.sh
+++ eclimd/run-eclimd.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+# script to launch eclimd
+
+# Map current directory to workspace, and create a folder for eclipse
+# to use to store plugins etc. Allowing for specific eclipse config
+# per project.
+ECLIPSE_PLUGINS_DIR=.eclipse-docker
+WORKDIR=`pwd`
+
 docker run \
        --rm \
        -it \
-       eclimd \
-       /usr/bin/xvfb-run /usr/local/eclipse/eclimd -b  "$@"
+       -v $WORKDIR/$ECLIPSE_PLUGINS_DIR:/home/dev/.eclipse \
+       -v $WORKDIR:/home/dev/workspace \
+       -p 9091:9091 \
+       eclim \
+       /usr/bin/xvfb-run /usr/local/eclipse/eclimd  "$@"
