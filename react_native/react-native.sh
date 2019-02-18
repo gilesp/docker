b744845549f297449491301bb702d5ac5a94760c
Giles Paterson <giles.paterson@blackpepper.co.uk>
2019-02-18 16:57:27 +0000

    Deleted obsolete images

--- react_native/react-native.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker run \
-       --rm \
-       -it \
-       --privileged \
-       -v /dev/bus/usb:/dev/bus/usb \
-       -v $(pwd):/home/dev/app \
-       react-native \
-       react-native "$@"
